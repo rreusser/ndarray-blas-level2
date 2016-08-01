3ceeb7e371dfd72fa025727a25ec8b4f5c7b439e
Tim Bright <capoeirista.muralha@gmail.com>
2016-07-31 20:37:00 -0700

    Added SBMV function and unit tests.

--- sbmv.js
+++ sbmv.js
@@ -1,7 +1,57 @@
 'use strict';
 
+var blas1 = require('ndarray-blas-level1');
+
 module.exports = sbmv;
 
-function sbmv () {
-  console.error('SBMV (symmetric banded matrix vector multiply) not yet implemented');
+function sbmv (A, k, x, y, fromLower, alpha, beta) {
+  var n = A.shape[0];
+
+  var lower = fromLower || true;
+  var alpha0 = alpha === undefined ? 1 : alpha;
+  var beta0 = beta === undefined ? 0 : beta;
+
+  var i = 0;
+  var j = 0;
+  if (beta0 === 0) {
+    for (i = 0; i < y.shape[0]; ++i) {
+      y.set(i, 0);
+    }
+  } else if (beta0 !== 1) {
+    blas1.scal(beta0, y);
+  }
+  if (alpha0 === 0) {
+    return true;
+  }
+
+  var temp1 = 0.0;
+  var temp2 = 0.0;
+  var l = 0.0;
+  var kplus1 = k + 1;
+  if (lower) {
+    for (j = 0; j < n; ++j) {
+      temp1 = alpha0 * x.get(j);
+      temp2 = 0;
+      y.set(j, y.get(j) + temp1 * A.get(0, j));
+      l = 1 - j;
+      for (i = j + 1; i < Math.min(n, j + k); ++i) {
+        y.set(i, y.get(i) + temp1 * A.get(l + i, j));
+        temp2 = temp2 + A.get(l + i, j) * x.get(i);
+      }
+      y.set(j, y.get(j) + alpha0 * temp2);
+    }
+  } else {
+    for (j = 0; j < n; ++j) {
+      temp1 = alpha0 * x.get(j);
+      temp2 = 0;
+      l = kplus1 - j;
+      for (i = Math.max(l, j - k); i < j - 1; ++i) {
+        y.set(i, y.get(i) + temp1 * A.get(l + i, j));
+        temp2 = temp2 + A.get(l + i, j) * x.get(i);
+      }
+      y.set(j, y.get(j) + temp1 * A.get(kplus1, j) + alpha0 * temp2);
+    }
+  }
+
+  return true;
 }
---
+++ test/sbmv.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var RandMatGen = require('./util/rand-matrix-gen.js');
+var ndarray = require('ndarray');
+var assertCloseTo = require('./util/close-to');
+var constants = require('./util/constants');
+
+var gemv = require('../gemv');
+var sbmv = require('../sbmv');
+
+describe('SBMV (symmetric banded matrix-vector product)', function () {
+  var n = 15;
+  var k = 6;
+  var seed;
+  var matGen = new RandMatGen(seed, Float64Array);
+  var x = ndarray(new Float64Array(n), [n]);
+  var x0 = ndarray(new Float64Array(n), [n]);
+  var xn = ndarray(new Float64Array(n), [n]);
+  var B = ndarray(new Float64Array(n * n), [n, n]);
+
+  it('upper-triangular sbmv', function () {
+    for (var t = 0; t < constants.NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeSymmBandedMatrix(n, k, B);
+      sbmv(B, k, x0, x);
+      gemv(1, B, x0, 0, xn);
+      assertCloseTo(x, xn, constants.TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
+  });
+  it('lower-triangular sbmv', function () {
+    for (var t = 0; t < constants.NUM_TESTS; ++t) {
+      seed = matGen.setRandomSeed(36);
+      matGen.makeSymmBandedMatrix(n, k, B);
+      sbmv(B, k, x0, x, false);
+      gemv(1, B, x0, 0, xn);
+      assertCloseTo(x, xn, constants.TEST_TOLERANCE, 'Failure seed value: "' + seed + '".');
+    }
+  });
+});
